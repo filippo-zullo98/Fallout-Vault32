92fa3b32f6c4c7ebb400eec2e867613da8eb9d51
filippo-zullo98 <zullo.filippo98@gmail.com>
2025-12-26 19:16:08 +0100

    Risolto il problema della fluidità dei personaggi su monitor

--- Frontend/src/main/java/frontend/GamePanel.java
+++ Frontend/src/main/java/frontend/GamePanel.java
@@ -58,44 +58,40 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     @Override
-    public void run() { // metodo che viene chiamato da Thread per avviare il processo concorrente (thread)
-        double drawInterval = 1000000000/FPS;
-        double nextDrawTime = System.nanoTime() + drawInterval;
-        // Creo qui un ciclo di gioco, che sarà il fulcro del gioco
-        while (gameThread != null) {
-            // elenco operazioni del thread
-            // 1 AGGIORNAMENTO: aggiorna informazioni come la posizione dei personaggi
-            update();
-            // 2 DISEGNO: disegna sullo schermo con le informazioni aggiornate
-            repaint();
+    public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime /= 1000000;
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-                Thread.sleep((long) remainingTime);
-                nextDrawTime += drawInterval;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
             }
         }
     }
+
     // creo il metodo dedicato all'operazione 1 di aggiornamento
     // i valori X aumentano lo spostamento a dx
     // i valori Y aumentano lo spostamento in basso
     public void update() {
-        if (keyH.upPressed == true) {
+        if (keyH.upPressed) {
             playerY -= playerSpeed;
         }
-        else if (keyH.downPressed == true) {
+        else if (keyH.downPressed) {
             playerY += playerSpeed;
         }
-        else if (keyH.leftPressed == true){
+        else if (keyH.leftPressed){
             playerX -= playerSpeed;
         }
-        else if (keyH.rightPressed == true) {
+        else if (keyH.rightPressed) {
             playerX += playerSpeed;
         }
     }
@@ -103,8 +99,12 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // pulisce il pannello prima di ridisegnare
         Graphics2D g2 = (Graphics2D)g; // trasforma l'oggetto Graphics in Graphics 2D
+
         g2.setColor(Color.white);
         g2.fillRect(playerX, playerY, titleSize, titleSize); // disegna un quadrato bianco di 48x48 pixel alle coordinate (100, 100)
+
         g2.dispose(); // rilascia le risorse di sistema usate per disegnare subito dopo aver terminato
+
+        Toolkit.getDefaultToolkit().sync(); // questo risolve il problema della fluidità del movimento
     }
 }
