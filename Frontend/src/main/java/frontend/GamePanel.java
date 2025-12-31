2e5f72c0436770a322ab0f2688fd2ca6503d5ce0
filippo-zullo98 <zullo.filippo98@gmail.com>
2025-12-31 20:22:31 +0100

    Modifica risoluzione e formato dei personaggi in 32x32 pixel

--- Frontend/src/main/java/frontend/GamePanel.java
+++ Frontend/src/main/java/frontend/GamePanel.java
@@ -1,4 +1,6 @@
 package frontend;
+import entity.Player;
+
 import javax.swing.JPanel;
 import java.awt.*;
 
@@ -7,17 +9,17 @@ public class GamePanel extends JPanel implements Runnable{
     // originalTitleSize: 16x16 è la dimensione dei .png del gioco (standard per i giochi in 2D)
     // tuttavia, le moderne risoluzioni dei pc renderebbero troppo piccoli i .png 16x16
     // occorre ridimensionare i .png (far sembrare i 16x16 in 48x48 a schermo)
-    final int originalTitleSize = 16;
-    final int scale = 3;
-    final int titleSize = originalTitleSize * scale; // cosi i 16x16 diventano 48x48
+    final int originalTileSize = 32;
+    final int scale = 2;
+    public final int tileSize = originalTileSize * scale; // diventano 32 * 2 = 64px
 
     // dimensione della nostra schermata di gioco: Quante tessere/.png il gioco può visualizzare sia orizzontalmente che verticalmente?
     // questa misura determinerà il numero di tessere visualizzabili: 16 orizzontalmente e 12 verticalmente
     // il rapporto è 4:3
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = titleSize * maxScreenCol; // 768 pixels
-    final int screenHeight = titleSize * maxScreenRow; // 576 pixels
+    final int screenWidth = tileSize * maxScreenCol; // 64 * 16 = 1024 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 64*12 = 768 pixels
     /*
     * PROBLEMA ANIMAZIONE
     * Il gioco ha bisogno del suo "clock" per poter far si che le immagini possano "muoversi".
@@ -25,6 +27,7 @@ public class GamePanel extends JPanel implements Runnable{
     * all'interno del programma.
     * */
     Thread gameThread;
+
     /*
     * PROBLEMA: CONTROLLO DEI PERSONAGGI DA TASTIERA
     * Il gioco ha bisogno di un keyHandler che riesca a stabilire quando il giocatore spinge dei tasti per muovere
@@ -32,6 +35,7 @@ public class GamePanel extends JPanel implements Runnable{
     * A tal proposito creo un oggetto della classe KeyHandler.
     * */
     KeyHandler keyH = new KeyHandler();
+    Player player = new Player(this, keyH);
 
     // impostiamo la posizione di default del giocatore
     int playerX = 100;
@@ -82,26 +86,14 @@ public class GamePanel extends JPanel implements Runnable{
     // i valori X aumentano lo spostamento a dx
     // i valori Y aumentano lo spostamento in basso
     public void update() {
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        else if (keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        else if (keyH.leftPressed){
-            playerX -= playerSpeed;
-        }
-        else if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+       player.update();
     }
     // creo il metodo dedicato all'operazione 1 di rappresentazione/disegno delle componenti/personaggi
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // pulisce il pannello prima di ridisegnare
         Graphics2D g2 = (Graphics2D)g; // trasforma l'oggetto Graphics in Graphics 2D
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, titleSize, titleSize); // disegna un quadrato bianco di 48x48 pixel alle coordinate (100, 100)
+        player.draw(g2);
 
         g2.dispose(); // rilascia le risorse di sistema usate per disegnare subito dopo aver terminato
 
